53f3ed355f33fd278134839fd0d1030c5ede33aa
Wiki92x <gemissnasassben@gmail.com>
2025-04-13 09:54:33 +0400

    Update wagmiClient.ts

--- lib/wagmiClient.ts
+++ lib/wagmiClient.ts
@@ -1,27 +1,13 @@
+// lib/wagmiClient.ts
 'use client';
 
-import { createConfig, configureChains } from 'wagmi';
+import { configureChains, createConfig } from 'wagmi';
 import { mainnet, polygon, bsc } from 'wagmi/chains';
-import { jsonRpcProvider } from 'wagmi/providers/jsonRpc';
+import { publicProvider } from 'wagmi/providers/public';
 
 const { chains, publicClient } = configureChains(
   [mainnet, polygon, bsc],
-  [
-    jsonRpcProvider({
-      rpc: (chain) => {
-        switch (chain.id) {
-          case mainnet.id:
-            return { http: 'https://eth.llamarpc.com' };
-          case polygon.id:
-            return { http: 'https://polygon-rpc.com' };
-          case bsc.id:
-            return { http: 'https://bsc-dataseed.binance.org/' };
-          default:
-            return null;
-        }
-      },
-    }),
-  ]
+  [publicProvider()]
 );
 
 const wagmiConfig = createConfig({
