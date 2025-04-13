a9b3b6b8a0b4f59012407fb1b6c44d7466bb1331
Wiki92x <gemissnasassben@gmail.com>
2025-04-13 09:25:34 +0400

    Update wagmiClient.ts

--- lib/wagmiClient.ts
+++ lib/wagmiClient.ts
@@ -4,15 +4,19 @@ import { createConfig, configureChains } from 'wagmi';
 import { mainnet, polygon, bsc } from 'wagmi/chains';
 import { publicProvider } from 'wagmi/providers/public';
 
-const { chains, publicClient, webSocketPublicClient } = configureChains(
-  [mainnet, polygon, bsc],
-  [publicProvider()]
-);
+let wagmiConfig;
 
-const wagmiConfig = createConfig({
-  autoConnect: true,
-  publicClient,
-  webSocketPublicClient,
-});
+if (typeof window !== 'undefined') {
+  const { chains, publicClient, webSocketPublicClient } = configureChains(
+    [mainnet, polygon, bsc],
+    [publicProvider()]
+  );
+
+  wagmiConfig = createConfig({
+    autoConnect: true,
+    publicClient,
+    webSocketPublicClient,
+  });
+}
 
 export default wagmiConfig;
