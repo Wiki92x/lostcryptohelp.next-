00190107d029a5985a8698f1ace436d8ef59efb0
Wiki92x <gemissnasassben@gmail.com>
2025-04-13 08:05:52 +0400

    Update wagmiClient.ts

--- lib/wagmiClient.ts
+++ lib/wagmiClient.ts
@@ -4,15 +4,22 @@ import { createConfig, configureChains } from 'wagmi';
 import { publicProvider } from 'wagmi/providers/public';
 import { mainnet, bsc, polygon } from 'wagmi/chains';
 
-const { chains, publicClient, webSocketPublicClient } = configureChains(
-  [mainnet, bsc, polygon],
-  [publicProvider()]
-);
+// Prevents build-time SSR error on Netlify
+const isBrowser = typeof window !== 'undefined';
 
-const wagmiConfig = createConfig({
-  autoConnect: true,
-  publicClient,
-  webSocketPublicClient,
-});
+let wagmiConfig: any = undefined;
+
+if (isBrowser) {
+  const { chains, publicClient, webSocketPublicClient } = configureChains(
+    [mainnet, bsc, polygon],
+    [publicProvider()]
+  );
+
+  wagmiConfig = createConfig({
+    autoConnect: true,
+    publicClient,
+    webSocketPublicClient,
+  });
+}
 
 export default wagmiConfig;
