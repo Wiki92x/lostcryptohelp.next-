f4836ad8744fe9d98c810d994e999cd4319e9dee
Wiki92x <gemissnasassben@gmail.com>
2025-04-14 09:51:32 +0400

    Update providers.tsx

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,13 +1,14 @@
 'use client';
+
 import { useEffect } from 'react';
 import { usePathname } from 'next/navigation';
 import { pageview } from '@/lib/ga';
 
-export function AnalyticsProvider() {
+export default function Providers() {
   const pathname = usePathname();
 
   useEffect(() => {
-    pageview(pathname);
+    if (pathname) pageview(pathname);
   }, [pathname]);
 
   return null;
