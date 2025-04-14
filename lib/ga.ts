3a7c2872a6f89805d8bbcb3b7ec740cdcbc0b0b8
Wiki92x <gemissnasassben@gmail.com>
2025-04-14 09:38:32 +0400

    Update ga.ts

--- lib/ga.ts
+++ lib/ga.ts
@@ -1,6 +1,9 @@
+// lib/ga.ts
 export const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_ID || 'G-XXXXXXX';
 
 export const pageview = (url: string) => {
   if (typeof window.gtag !== 'function') return;
-  window.gtag('config', GA_TRACKING_ID, { page_path: url });
+  window.gtag('config', GA_TRACKING_ID, {
+    page_path: url,
+  });
 };
