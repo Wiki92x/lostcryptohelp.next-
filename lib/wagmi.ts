daa5ceeb80650bee964ee7eb7eec83a18ac38cf0
Wiki92x <gemissnasassben@gmail.com>
2025-04-14 10:11:49 +0400

    Update wagmi.ts

--- lib/wagmi.ts
+++ lib/wagmi.ts
@@ -1,27 +1,13 @@
-import { configureChains, createConfig } from 'wagmi';
-import { mainnet, polygon, bsc, arbitrum } from 'wagmi/chains';
+// lib/wagmi.ts
+'use client';
+
+import { createConfig, configureChains } from 'wagmi';
+import { mainnet, bsc } from 'wagmi/chains';
 import { publicProvider } from 'wagmi/providers/public';
 
 const { chains, publicClient, webSocketPublicClient } = configureChains(
-  [mainnet, polygon, bsc, arbitrum],
-  [
-    publicProvider({
-      rpc: (chain) => {
-        switch (chain.id) {
-          case 1:
-            return { http: 'https://eth.llamarpc.com' };
-          case 56:
-            return { http: 'https://bsc-dataseed.binance.org' };
-          case 137:
-            return { http: 'https://polygon-rpc.com' };
-          case 42161:
-            return { http: 'https://arb1.arbitrum.io/rpc' };
-          default:
-            throw new Error(`Unsupported chain ${chain.id}`);
-        }
-      },
-    }),
-  ]
+  [mainnet, bsc],
+  [publicProvider()]
 );
 
 export const wagmiConfig = createConfig({
