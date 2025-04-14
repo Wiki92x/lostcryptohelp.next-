85911334ba6ea8d76df9cdcd26dd514c075dee72
Wiki92x <gemissnasassben@gmail.com>
2025-04-14 09:35:18 +0400

    Create ga.ts

---
+++ lib/ga.ts
@@ -0,0 +1,6 @@
+export const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_ID || 'G-XXXXXXX';
+
+export const pageview = (url: string) => {
+  if (typeof window.gtag !== 'function') return;
+  window.gtag('config', GA_TRACKING_ID, { page_path: url });
+};
