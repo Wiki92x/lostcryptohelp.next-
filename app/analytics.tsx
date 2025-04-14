6438925e52aa667f07fd3edcdf5e5d30b37f8ca8
Wiki92x <gemissnasassben@gmail.com>
2025-04-14 09:49:02 +0400

    Update analytics.tsx

--- app/analytics.tsx
+++ app/analytics.tsx
@@ -4,13 +4,14 @@ import { useEffect } from 'react';
 import { usePathname, useSearchParams } from 'next/navigation';
 import { pageview } from '@/lib/ga';
 
-
 export default function Analytics() {
   const pathname = usePathname();
   const searchParams = useSearchParams();
 
   useEffect(() => {
-    const url = pathname + searchParams.toString();
+    if (!pathname || !searchParams) return;
+    const query = searchParams.toString();
+    const url = pathname + (query ? `?${query}` : '');
     pageview(url);
   }, [pathname, searchParams]);
 
