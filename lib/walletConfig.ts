a36cc419d321d05757167541469b6443f25a38cd
Wiki92x <gemissnasassben@gmail.com>
2025-04-14 11:26:45 +0400

    Update walletConfig.ts

--- lib/walletConfig.ts
+++ lib/walletConfig.ts
@@ -6,7 +6,7 @@ import { publicProvider } from 'wagmi/providers/public';
 export function getWalletConfig() {
   const { publicClient, webSocketPublicClient } = configureChains(
     [mainnet, bsc, polygon],
-    [publicProvider()] // Safe fallback provider
+    [publicProvider()] // avoid using broken/undefined providers at build
   );
 
   return createConfig({
