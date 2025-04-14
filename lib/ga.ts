9acd37917082614857e2c4a9f9d51b21e8a1837b
Wiki92x <gemissnasassben@gmail.com>
2025-04-14 09:56:27 +0400

    Update ga.ts

--- lib/ga.ts
+++ lib/ga.ts
@@ -1,5 +1,11 @@
-// lib/ga.ts
-export const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_ID || 'G-XXXXXXX';
+export const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_ID || '';
+
+// 👇 Extend window interface to include gtag
+declare global {
+  interface Window {
+    gtag: (...args: any[]) => void;
+  }
+}
 
 export const pageview = (url: string) => {
   if (typeof window.gtag !== 'function') return;
